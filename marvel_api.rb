9fd3b6b94743495f7dc4723f5f0f3533922e7730
AJAYJAITWAL <ajayjaitwal3434@gmil.com>
2022-11-13 15:11:12 +0530

    Add api for get list of all characters

--- marvel_api.rb
+++ marvel_api.rb
@@ -1,2 +1,24 @@
+# require gems
+require 'faraday'
+require 'dotenv'
+Dotenv.load
+
 class MarvelApi
+
+  # list of all the characters
+  def get_all_characters_list
+    Faraday.get("#{ENV["BASE_URL"]}/v1/public/characters?ts=#{get_timestamp}&apikey=#{ENV["PUBLIC_KEY"]}&hash=#{get_hash}")
+  end
+
+  private
+
+  # get uniq time stemp
+  def get_timestamp
+    Time.now.strftime("%d%m%Y%H%M%S")
+  end
+
+  # get digest hash
+  def get_hash
+    Digest::MD5.hexdigest("#{get_ts}#{ENV["PRIVATE_KEY"]}#{ENV["PUBLIC_KEY"]}")
+  end
 end
