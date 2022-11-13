2eb4489a0a4e520ad3154b6d8075b15758a680db
AJAYJAITWAL <ajayjaitwal3434@gmil.com>
2022-11-13 15:20:39 +0530

    Add api for get character record

--- marvel_api.rb
+++ marvel_api.rb
@@ -5,9 +5,20 @@ Dotenv.load
 
 class MarvelApi
 
-  # list of all the characters
+  attr_accessor :params
+
+  def initialize(params=nil)
+    @params = params
+  end
+
+  # list of all the characters records
   def get_all_characters_list
     Faraday.get("#{ENV["BASE_URL"]}/v1/public/characters?ts=#{get_timestamp}&apikey=#{ENV["PUBLIC_KEY"]}&hash=#{get_hash}")
+  end
+
+  # get only character record
+  def get_character
+    Faraday.get("#{ENV["BASE_URL"]}/v1/public/characters/#{params[:character_id]}?ts=#{get_timestamp}&apikey=#{ENV["PUBLIC_KEY"]}&hash=#{get_hash}")
   end
 
   private
@@ -19,6 +30,6 @@ class MarvelApi
 
   # get digest hash
   def get_hash
-    Digest::MD5.hexdigest("#{get_ts}#{ENV["PRIVATE_KEY"]}#{ENV["PUBLIC_KEY"]}")
+    Digest::MD5.hexdigest("#{get_timestamp}#{ENV["PRIVATE_KEY"]}#{ENV["PUBLIC_KEY"]}")
   end
 end
